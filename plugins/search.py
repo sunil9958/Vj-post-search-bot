de2482be9022e2be7c9b30044db78d39eb17e8ef
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-07-21 23:14:13 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -41,7 +41,7 @@ async def search(bot, message):
     if message.text.startswith("/"):
        return    
     query   = message.text 
-    head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@VJ_Botz ❗</I></b>\n\n"
+    head    = f"<u>⭕ Here is the results {message.from_user.mention} 👇\n\n💢 Powered By </u> <b><I>@SvFilmsX_Official @SvfilmsX_Adultzone ❗</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
@@ -84,7 +84,7 @@ async def recheck(bot, update):
     id      = update.data.split("_")[-1]
     query   = await search_imdb(id)
     channels = (await get_group(update.message.chat.id))["channels"]
-    head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I>@VJ_Botz ❗</I></b>\n\n"
+    head    = "<u>⭕ I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\n💢 Powered By </u> <b><I>@SvFilmsX_Official https://t.me/+Z3CJbPbb_u5mOGNl ❗</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
