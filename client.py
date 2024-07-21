45de66d8ec3be64ea3e8d1875704d6c1a8cb454c
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-07-21 23:02:53 +0530

    Update client.py

--- client.py
+++ client.py
@@ -15,6 +15,6 @@ class Bot(Client):
             plugins={"root": "plugins"})
     async def start(self):                        
         await super().start()  
-        print("Bot Started 🔧 Powered By @VJ_Botz")   
+        print("Bot Started 🔧 Powered By @SvFilmsX_Official")   
     async def stop(self, *args):
         await super().stop()
