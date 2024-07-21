0466b459ab121d703ec115cb232f29be18d7ff43
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-07-21 23:10:36 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -18,8 +18,8 @@ async def start(bot, message):
         InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
         InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
     ],[
-        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/SvFilmsX_update"),
+        InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/+Z3CJbPbb_u5mOGNl")
     ]]
     await message.reply(text=script.START.format(message.from_user.mention),
                         disable_web_page_preview=True,
@@ -66,8 +66,8 @@ async def misc(bot, update):
            InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
            InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
        ],[
-           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-           InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/SvFilmsX_update"),
+           InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/+Z3CJbPbb_u5mOGNl")
        ]]
        await update.message.edit(text=script.START.format(update.from_user.mention),
                                  disable_web_page_preview=True,
